6d33216d32b6f0bff60a933e13448a309c0cfaed
usaramazan <60402002+usaramazan@users.noreply.github.com>
2020-05-08 15:39:46 -0400

    Update AddDedim.java

--- src/day_1/AddDedim.java
+++ src/day_1/AddDedim.java
@@ -5,5 +5,6 @@ public class AddDedim {
         System.out.println("add demeden direct puch diycem olamzsa commit and push deniycem");
         System.out.println("direct git puch olmadi simdi commit deniycem");
         //add . demezsen sadece class i ekliyor, icerik eklenmiyor
+        // bunu inetlliJ den call etmek icin git puul yada doctor isareti click
     }
 }
