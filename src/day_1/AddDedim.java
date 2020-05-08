42baa36806b31b8a49d14efd4a1253fc49c2e977
Ramazan Aksit <usaramazan@gmail.com>
2020-05-08 15:32:18 -0400

    updated

--- src/day_1/AddDedim.java
+++ src/day_1/AddDedim.java
@@ -1,4 +1,8 @@
 package day_1;
 
 public class AddDedim {
+    public static void main(String[] args) {
+        System.out.println("add demeden direct puch diycem olamzsa commit and push deniycem");
+        System.out.println("direct git puch olmadi simdi commit deniycem");
+    }
 }
